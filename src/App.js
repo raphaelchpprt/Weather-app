72e5004ee9a9d991195be2527815e09a7853c168
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-07-02 12:54:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
+//Styles
 import './App.css';
+
+//Components
+import WeekContainer from './components/WeekContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="appTitle" className="mt-4">
+        <b>5-Day Forecast</b>
+      </h1>
+      <WeekContainer />
     </div>
   );
 }
